094246d344beeb11dfe90da437da55c5d267db67
JamesShabo <jamesshabo@Jamess-MacBook-Pro.local>
2016-10-17 15:35:48 -0700

    Changed clock to regular time

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var d,h,m,s,color;
+var d,h,m,s,time;
 
 function displayTime(){
 	d = new Date();
@@ -6,13 +6,22 @@ function displayTime(){
 	m = d.getMinutes();
 	s = d.getSeconds();
 
-	if(h <= 9) h = '0'+h;
+	if(h > 12) h = h - 12;
 	if(m <= 9) m = '0'+m;
 	if(s <= 9) s = '0'+s;
+   
+ 
+    
+	
+    
 
-	color = "#" +h+m+s;
-	document.body.style.background = color;
-	document.getElementById("clock").innerHTML = color;
+
+	time = h + ':' + m + ':' + s
+	document.body.style = time;
+	document.getElementById("clock").innerHTML = time;
 
 	setTimeout(displayTime, 1000)
 }	displayTime();
+//add colons
+//change to regular time
+// 
